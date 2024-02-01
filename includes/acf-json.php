08e3dbad0dec96b35970ca9dccb8e05324d38933
Charlene <charlenelcapili@gmail.com>
2024-02-01 00:20:13 -0500

    acf json include cleanup

--- includes/acf-json.php
+++ includes/acf-json.php
@@ -7,9 +7,6 @@
 
 add_filter( 'acf/json/load_paths', 'custom_acf_blocks_json_load_paths' );
 add_filter( 'acf/settings/save_json/type=acf-field-group', 'custom_acf_blocks_json_save_path_for_field_groups' );
-add_filter( 'acf/settings/save_json/type=acf-ui-options-page', 'custom_acf_blocks_json_save_path_for_option_pages' );
-add_filter( 'acf/settings/save_json/type=acf-post-type', 'custom_acf_blocks_json_save_path_for_post_types' );
-add_filter( 'acf/settings/save_json/type=acf-taxonomy', 'custom_acf_blocks_json_save_path_for_taxonomies' );
 add_filter( 'acf/json/save_file_name', 'custom_acf_blocks_json_filename', 10, 3 );
 
 /**
@@ -25,25 +22,8 @@ add_filter( 'acf/json/save_file_name', 'custom_acf_blocks_json_filename', 10, 3 
  */
 function custom_acf_blocks_json_load_paths( $paths ) {
 	$paths[] = CUSTOM_ACF_BLOCKS_PLUGIN_DIR . '/acf-json/field-groups';
-	$paths[] = CUSTOM_ACF_BLOCKS_PLUGIN_DIR . '/acf-json/options-pages';
-	$paths[] = CUSTOM_ACF_BLOCKS_PLUGIN_DIR . '/acf-json/post-types';
-	$paths[] = CUSTOM_ACF_BLOCKS_PLUGIN_DIR . '/acf-json/taxonomies';
 
 	return $paths;
-}
-
-/**
- * Set custom ACF JSON save point for
- * ACF generated post types.
- *
- * @link https://www.advancedcustomfields.com/resources/local-json/#saving-explained
- *
- * @return string $path New, outgoing path.
- *
- * @since 0.1.1
- */
-function custom_acf_blocks_json_save_path_for_post_types() {
-	return CUSTOM_ACF_BLOCKS_PLUGIN_DIR . '/acf-json/post-types';
 }
 
 /**
@@ -58,34 +38,6 @@ function custom_acf_blocks_json_save_path_for_post_types() {
  */
 function custom_acf_blocks_json_save_path_for_field_groups() {
 	return CUSTOM_ACF_BLOCKS_PLUGIN_DIR . '/acf-json/field-groups';
-}
-
-/**
- * Set custom ACF JSON save point for
- * ACF generated taxonomies.
- *
- * @link https://www.advancedcustomfields.com/resources/local-json/#saving-explained
- *
- * @return string $path New, outgoing path.
- *
- * @since 0.1.1
- */
-function custom_acf_blocks_json_save_path_for_taxonomies() {
-	return CUSTOM_ACF_BLOCKS_PLUGIN_DIR . '/acf-json/taxonomies';
-}
-
-/**
- * Set custom ACF JSON save point for
- * ACF generated Options Pages.
- *
- * @link https://www.advancedcustomfields.com/resources/local-json/#saving-explained
- *
- * @return string $path New, outgoing path.
- *
- * @since 0.1.1
- */
-function custom_acf_blocks_json_save_path_for_option_pages() {
-	return CUSTOM_ACF_BLOCKS_PLUGIN_DIR . '/acf-json/options-pages';
 }
 
 /**
